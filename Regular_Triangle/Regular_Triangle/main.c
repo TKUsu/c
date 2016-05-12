d9aa97b5d289cae1e2767a884b0251a9986ee518
TKUsu <202410089@s02.tku.edu.tw>
2016-05-13 04:57:07 +0800

    Regular Triangle reduce code

--- Regular_Triangle/Regular_Triangle/main.c
+++ Regular_Triangle/Regular_Triangle/main.c
@@ -8,36 +8,35 @@
 #include <stdio.h>
 
 //印空白
-int temp(int i){
+void temp(int i){
     if (i > 0){
         printf(" ");
-        return temp(i-1);
-    }return 0;
+        temp(i-1);
+    }
 }
 //印右下三角形，0為起點
-int temp2(int i,int j){
+void temp2(int i,int j){
     if (i < j){
         printf("*");
-        return temp2(i+1,j);
-    }return 0;
+        temp2(i+1,j);
+    }
 }
 //印靠左三角形，1為起點
-int temp3(int i,int j){
+void temp3(int i,int j){
     if (i <= j){
         printf("*");
-        return temp3(i+1,j);
-    }return 0;
+        temp3(i+1,j);
+    }
 }
 
-int A(int i,int j){
+void A(int i,int j){
     if(i > 0){
         temp(i-1);
         temp2(i,j);
         temp3(i,j);
         printf("\n");
-        return A(i-1,j);
+        A(i-1,j);
     }
-    return 0;
 }
 
 int main(int argc, const char * argv[]) {
